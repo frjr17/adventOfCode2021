1b5e93ac3d612ac266e66f78ffa711d8a4c663d7
Hernan Valencia <frjr17@criptext.com>
2022-03-18 19:41:19 -0500

    Finishing day 3

--- main.py
+++ main.py
@@ -1,57 +1,80 @@
 with open("./puzzle.txt") as file:
-    puzzle = file.read().splitlines()
+    fpuzzle = file.read().splitlines()
+
     # print(puzzle)
 
 
-resp = input("Part 1 or 2?")
+resp = 2
 
+puzzle = [[number for number in binary] for binary in fpuzzle]
+
+
+def get_positions(puzzle):
+    positions = {}
+    i = 0
+    for binary in puzzle:
+        j = 0
+        for number in binary:
+            try:
+                positions[j] += number
+            except KeyError:
+                positions[j] = number
+            j += 1
+
+        i += 1
+    return positions
+
+def get_rating(puzzle,number,i,reverse):
+    rating = []
+    comp_num = '1' if not reverse == True else '0'
+    print(comp_num)
+    comp_num_r = '0' if not reverse == True else '1'
+    if number.count('1')>= number.count('0'):
+        for binary in puzzle:
+            if binary[i] == comp_num:
+                rating.append(binary)
+    else:
+        for binary in puzzle:
+            if binary[i] == comp_num_r:
+                rating.append(binary)
+    return rating
+        
 if resp == '1':
     print("Part 1")
-    dict1 = {}
-    incr = 0
-    decr = 0
-    i = 0
-
-    for number in puzzle:
-        if i != 0:
-            before_number = puzzle[i-1]
-            if number > before_number:
-                print(number, 'increased from', before_number)
-                dict1[number] = 'increased'
-                incr += 1
-            else:
-                print(number, 'decreased from', before_number)
-                dict1[number] = 'decreased'
-                decr += 1
-        i += 1
-
-    print('Increased:', incr, '. Decreased:', decr)
-    # First Answer: 1529 (The program runs 1530 because of a puzzle error)
+    gamma_rates = []
+    epsilon_rates = []
+    positions = get_positions(puzzle)
+    for i, number in positions.items():
+        if number.count('1') > number.count("0"):
+            gamma_rates.append('1')
+            epsilon_rates.append('0')
+        else:
+            gamma_rates.append('0')
+            epsilon_rates.append('1')
+    gamma = ''.join(gamma_rates)
+    epsilon = ''.join(epsilon_rates)
+    print(f"Gamma: {gamma} = {int(gamma,2)}")
+    print(f"Epsilon: {epsilon} = {int(epsilon,2)} ")
+    print(f"Power Consumption: {int(gamma,2)*int(epsilon,2)}")
 else:
     print('Part 2')
-    windows = {}
-    incr = 0
-    decr = 0
-    i = 0
-
-    for number in puzzle:
-        windows[i] = {}
-        if i != 0:
-            try:
-                number2 = puzzle[i+1]
-                number3 = puzzle[i+2]
-                windows[i]['quantity'] = int(number)+int(number2)+int(number3)
-                if windows[i]['quantity'] > windows[i-1]['quantity']:
-                    windows[i]['status'] = 'increased'
-                    incr += 1
-                else:
-                    windows[i]['status'] = 'decreased'
-                    decr += 1
-            except IndexError:
-                break
-        else:
-            windows[i]['quantity'] = 0
-        i += 1
-
-    print('Increased:', incr, '. Decreased:', decr)
-    # Second Answer: 1529 (The program runs 1530 because of a puzzle error)
+    o_puzzle = fpuzzle
+    o_rating = []
+    c_puzzle = fpuzzle
+    c_rating = []
+    for i in range(0, len(fpuzzle[0])):
+        o_number = get_positions(o_puzzle)[i]
+        c_number = get_positions(c_puzzle)[i]
+        o_rating = get_rating(o_puzzle,o_number,i,False)
+        c_rating = get_rating(c_puzzle,c_number,i,True)
+        if len(o_puzzle) > 1:
+            o_puzzle = o_rating
+            o_rating =[]
+        if len(c_puzzle) > 1:
+            c_puzzle = c_rating
+            c_rating =[]
+    o_rating = int(o_puzzle[0],2)
+    c_rating = int(c_puzzle[0],2)
+    print(f'Oxygen rating: {o_rating}')
+    print(f'CO2 rating: {c_rating}')
+    print(f'Life Support Rating: {o_rating*c_rating}')
