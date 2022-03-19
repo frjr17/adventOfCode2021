dc2b3b7d9c05220917191300f236566d38146432
Hernan Valencia <frjr17@criptext.com>
2022-03-19 01:17:25 -0500

    Finishing day 4

--- main.py
+++ main.py
@@ -1,80 +1,76 @@
+
+def filter_list(arr):
+    return list(filter(None, arr))
+
+
+def get_rows(chart):
+    return [filter_list(row.split(' ')) for row in chart]
+
+
+def get_colums(chart):
+    columns = [[row[i] for row in chart] for i in range(0, 5)]
+    return columns
+
+
+def print_line(l):
+    for x in l:
+        print(x)
+
+
+def detect_row_column(chart, numbers_played):
+    # Detect Rows
+    rows = chart
+    # Detect Columns
+    columns = get_colums(chart)
+    # Detect if there are mathed rows
+    for row in rows:
+        if set(row) <= set(numbers_played):
+            print('Row',row)
+            return row
+    for column in columns:
+        if set(column) <= set(numbers_played):
+            print('Row',row)
+            return column
+    return None
+
+
 with open("./puzzle.txt") as file:
-    fpuzzle = file.read().splitlines()
-
-    # print(puzzle)
-
-
-resp = 2
-
-puzzle = [[number for number in binary] for binary in fpuzzle]
-
-
-def get_positions(puzzle):
-    positions = {}
-    i = 0
-    for binary in puzzle:
-        j = 0
-        for number in binary:
-            try:
-                positions[j] += number
-            except KeyError:
-                positions[j] = number
-            j += 1
-
-        i += 1
-    return positions
-
-def get_rating(puzzle,number,i,reverse):
-    rating = []
-    comp_num = '1' if not reverse == True else '0'
-    print(comp_num)
-    comp_num_r = '0' if not reverse == True else '1'
-    if number.count('1')>= number.count('0'):
-        for binary in puzzle:
-            if binary[i] == comp_num:
-                rating.append(binary)
-    else:
-        for binary in puzzle:
-            if binary[i] == comp_num_r:
-                rating.append(binary)
-    return rating
-        
-if resp == '1':
+    puzzle = file.read().splitlines()
+    numbers = puzzle[0].split(",")
+    bingo_charts = filter_list([lines for lines in puzzle[2:]])
+    bingo_charts = filter_list([bingo_charts[i:i+5] for i in range(0,len(bingo_charts),5)])
+    bingo_charts = [[[number for number in filter_list(line.strip().split(" "))]for line in chart] for chart in bingo_charts]
+        # chart = puzzle[i:i+5]
+        # arr_chart = [[number for number in filter_list(line.strip().split(" "))] for line in chart]
+        # arr_chart = filter_list(arr_chart)
+        # bingo_charts.append(arr_chart)
+       
+part = '1'
+if part == '1':
     print("Part 1")
-    gamma_rates = []
-    epsilon_rates = []
-    positions = get_positions(puzzle)
-    for i, number in positions.items():
-        if number.count('1') > number.count("0"):
-            gamma_rates.append('1')
-            epsilon_rates.append('0')
-        else:
-            gamma_rates.append('0')
-            epsilon_rates.append('1')
-    gamma = ''.join(gamma_rates)
-    epsilon = ''.join(epsilon_rates)
-    print(f"Gamma: {gamma} = {int(gamma,2)}")
-    print(f"Epsilon: {epsilon} = {int(epsilon,2)} ")
-    print(f"Power Consumption: {int(gamma,2)*int(epsilon,2)}")
+
+    for i in range(0,len(numbers)):
+       
+        for j in range(0,len(bingo_charts)):
+            # print('Bingo Chart')
+            # print(j)
+            # print_line(bingo_charts[j])
+            # print('Numbers')
+            selected = detect_row_column(filter_list(bingo_charts[j]),numbers[0:i])
+            j+=1
+            if selected:
+                break
+        if selected:
+            break
+        i+=1
+    last_number = numbers[0:i][-1]
+    print('Winner Row:',selected)
+    print('Last Number:',last_number)
+    sum = 0
+    # Missing numbers unselected
+    # for number in selected:
+    #     sum += int(number)
+    print("Final Score:",int(last_number)*int(sum))
+
 else:
     print('Part 2')
-    o_puzzle = fpuzzle
-    o_rating = []
-    c_puzzle = fpuzzle
-    c_rating = []
-    for i in range(0, len(fpuzzle[0])):
-        o_number = get_positions(o_puzzle)[i]
-        c_number = get_positions(c_puzzle)[i]
-        o_rating = get_rating(o_puzzle,o_number,i,False)
-        c_rating = get_rating(c_puzzle,c_number,i,True)
-        if len(o_puzzle) > 1:
-            o_puzzle = o_rating
-            o_rating =[]
-        if len(c_puzzle) > 1:
-            c_puzzle = c_rating
-            c_rating =[]
-    o_rating = int(o_puzzle[0],2)
-    c_rating = int(c_puzzle[0],2)
-    print(f'Oxygen rating: {o_rating}')
-    print(f'CO2 rating: {c_rating}')
-    print(f'Life Support Rating: {o_rating*c_rating}')
