d5785f65626f36798b5dc78a4cf77b1ec44c8928
Hernan Valencia <frjr17@criptext.com>
2022-03-19 13:11:13 -0500

    Working to rebuild day5

--- main.py
+++ main.py
@@ -1,119 +1,55 @@
-
-from ast import Break
-from select import select
-
-
-def filter_list(arr):
-    return list(filter(None, arr))
-
-
-def get_rows(chart):
-    return [filter_list(row.split(' ')) for row in chart]
-
-
-def get_colums(chart):
-    columns = [[row[i] for row in chart] for i in range(0, 5)]
-    return columns
-
-
-def print_line(l):
-    for x in l:
-        print(x)
-    print("\n")
-
-
-def detect(chart, numbers_played):
-    rows = chart
-    selected = None
-    filtered_list = None
-    columns = get_colums(chart)
-
-    for row in rows:
-        if set(row) <= set(numbers_played):
-            # print("row:",row)
-            selected = row
-            break
-    for column in columns:
-        if set(column) <= set(numbers_played):
-            # print("column:",column)
-            selected = column
-            break
-
-    if selected:
-        filtered_list = filter_chart(chart,numbers_played)
-    
-    return [selected, filtered_list,chart]
-
-def filter_chart(chart,numbers_played):
-    flat_list = [number for line in chart for number in line]
-    filtered_list = []
-    for number in flat_list:
-        if number not in numbers_played:
-            filtered_list.append(number)
-    return filtered_list
-
 with open("./puzzle.txt") as file:
     puzzle = file.read().splitlines()
-    numbers = puzzle[0].split(",")
-    bingo_charts = filter_list([lines for lines in puzzle[2:]])
-    bingo_charts = filter_list([bingo_charts[i:i+5]
-                               for i in range(0, len(bingo_charts), 5)])
-    bingo_charts = [[[number for number in filter_list(
-        line.strip().split(" "))]for line in chart] for chart in bingo_charts]
 
-part = '2'
+    puzzle = [[[int(coord) for coord in coords.split(',')]
+               for coords in line.split(' -> ')]
+              for line in puzzle]
+
+    max_number = max([coord for line in puzzle
+                      for coords in line
+                      for coord in coords])+1
+
+    diagram = [('. '*max_number).strip().split(' ')
+               for x in range(0, max_number)]
+
+    # for line in diagram:
+    #     print(line)
+
+
+part = '1'
 if part == '1':
     print("Part 1")
+    overlaps = 0
+    for line in puzzle:
+        x1, y1 = line[0]
+        x2, y2 = line[1]
+        points = []
+        x_range = range(min(x1, x2), max(x1, x2)+1)
+        y_range = range(min(y1, y2), max(y1, y2)+1)
+        
+        if x1 == x2 or y1 == y2:
+            for x in x_range:
+                for y in y_range:
+                    point = [x, y]
+                    if point not in points:
+                        points.append(point)
 
-    for i in range(0, len(numbers)):
-        for j in range(0, len(bingo_charts)):
-            selected, ulist = detect(
-                filter_list(bingo_charts[j]), numbers[0:i])
-            j += 1
-            if selected:
-                break
-        if selected:
-            break
-        i += 1
+        for point in points:
+            print(point)
+            x, y = point
 
-    last_number = numbers[0:i][-1]
-    print('Winner Row:', selected)
-    print('Last Number:', last_number)
+            if diagram[y][x] == '.':
+                diagram[y][x] = 1
+            
+            if diagram[y][x] >= 1:
+                diagram[y][x] += 1
+                overlaps += 1
 
-    sum = 0
-    for number in ulist:
-        sum += int(number)
+    diagram = [[str(point) for point in line] for line in diagram]
 
-    print("Final Score:", int(last_number)*int(sum))
-
+    print('Final Diagram')
+    for line in diagram:
+        print(line)
+    print('\n Overlaps:',overlaps)
 else:
     print('Part 2')
-    filtered_charts = bingo_charts
-    i = 0
-    while True:
-        for j in range(0, len(bingo_charts)):
-            selected, ulist, chart = detect(filter_list(
-                filtered_charts[j]), numbers[0:i])
-            if selected:
-                print(len(filtered_charts))
-                if len(filtered_charts) >= 1:
-                    filtered_charts.remove(filtered_charts[j])
-                break
-            j += 1
-        if selected and len(filtered_charts) < 1:
-            break
-        i += 1
-        if i == len(numbers)-1:
-            i=0
-    last_number = numbers[0:i][-1]
-    print('Charts Available:',len(filtered_charts))
-    print('Last Row:', selected)
-    print('Last Chart:')
-    print_line(chart)
-    print('Last Number:', last_number)
-    print('Unused numbers:',ulist)
-    sum = 0
-    for number in ulist:
-        sum += int(number)
-
-    print("Final Score:", int(last_number)*int(sum))
